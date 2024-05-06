8ce154d39786c90f9b9332d87338f1cba745ced4
marie <marie@Maries-Laptop-E490>
2024-05-06 18:43:56 +0200

    changed some filenames (convergence)

--- 01_Project_steady_advection_diffusion/convergence2.m
+++ 01_Project_steady_advection_diffusion/convergence2.m
@@ -14,7 +14,6 @@
 close all; % Closing all our current figures
 clear; clc; % Clearing our Workspace and Command Window resp.
 
-
 %% 1. Setting up our first variables, Boundary conditions & Grid points
 
 PeG = 1; % Peclet Grid number
@@ -22,59 +21,39 @@ PeG = 1; % Peclet Grid number
 cL = 0; % Left BC
 cR = 1; % Right BC
 
-
 %% 2. Defining the vector `x` and the number of points `n`: ------------- %
 
 dx_list = [0.25, 0.125, 0.0625, 0.03125]; % Our test grid grid sizes
 
 x = [0.25, 0.5, 0.75]; % Our points of interest
 
-e_1 = zeros(1, length(dx_list)); % Initializing an Array of errors
+e_1 = zeros(1, length(dx_list)-1);
 
-%% 3. ...
-dx = 0.25/32
-n = 1/dx + 1;
-Pe_fine = PeG/dx;
-[c_num_fine, x_grid, PeG]  = ADEstationary(n, Pe_fine, cL, cR);
+%% 3. Calculate num soltion for dx=0.25
+dx = dx_list(1);
+n = 1/dx +1;
+Pe = PeG/dx;
+
+[c_num, x_grid, PeG] = ADEstationary(n, Pe,cL,cR);
 slicing_index = [0.25/dx + 1  :0.25/dx: n - 0.25/dx];
-c_num_sliced = c_num_fine(slicing_index);
+c_num_x_i = c_num(slicing_index);
+c_coarse = c_num_x_i;
 
+%% 4. Loop: Calculate error e_1=norm(c_coarse(x_i) - c_num(x_i)) between solutions of adjacent gridscized (compare with more coarse gridsize)
+for i= 2:(length(dx_list))
+    dx = dx_list(i);
+    n = 1/dx +1;
+    Pe = PeG/dx;
 
-%% 4. Computing the deviation e1 for each grid size dx: --------------------- %
-
-for j = 1:length(dx_list)
-
-    dx = dx_list(j);
-    Pe = PeG/dx; % Computing Péclet number from Péclet grid number
-    
-
-    % Implementing a slicing logic to only get the indices of interest
-    n = 1/dx + 1;
+    [c_num, x_grid, PeG] = ADEstationary(n, Pe,cL,cR);
     slicing_index = [0.25/dx + 1  :0.25/dx: n - 0.25/dx];
+    c_num_x_i = c_num(slicing_index);
+    e_1(i-1) = norm(c_coarse - c_num_x_i);
 
-    % For example if dx = 0.125, n = 1/0.125 + 1 = 9, which means that our
-    % nodes would be:
-    % [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-
-    % Since we are interessted in the nodes at the location x = [0.25, 0.5, 0.75], we are looking for
-    % the indices that contains these values.
-
-    % By slicing with [0.25/dx + 1  :0.25/dx: n - 0.25/dx] we get : [3 :2: 7]
-    % Which represents the array of indices at the values x = [0.25, 0.5, 0.75]
-
-    % Similarly, we do it for dx = 0.0625 -> slicing_index = [5 :4: 13]
-    % And so on...
-
-    %Numerical solution with n = 1/dx + 1 nodes
-    [c_num, x_grid, PeG]  = ADEstationary(n, Pe, cL, cR);
-
-    % Computing the deviationg e1 using the norm() function between the Analytical solution
-    % and the sliced Numerical solution:
-    e_1(j) = norm(c_num_sliced - c_num(slicing_index));    
-
+    c_coarse = c_num_x_i;
 end
 
-%% 5. Plotting the deviationg e1 for each grid size dx:  --------------------- %
+%% 5. Plot deviation of adjacent gridsizes against gridsize
 
 fig = figure; % creating a figure to later on save it
 set(fig, 'defaulttextinterpreter', 'latex') % enabling LaTex styling
@@ -84,7 +63,7 @@ set(fig, 'defaulttextinterpreter', 'latex') % enabling LaTex styling
 
 % 5.1. Using plot():
 subplot(2, 2, 1); % Creating a 2*2 subplot, selecting 1st
-plot(dx_list, e_1, 'x--', 'color', 'black', 'LineWidth', 1.5);
+plot(dx_list(2:end),e_1, 'x--', 'color', 'black', 'LineWidth', 1.5);
 
 xlabel('$\Delta x$ [-]');
 ylabel('$e_1$ [-]');
@@ -93,25 +72,25 @@ grid;
 
 % 5.2. Using semilogx():
 subplot(2, 2, 2);
-semilogx(dx_list,e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
-xlabel('$\Delta x$ [-]');
+semilogx(dx_list(2:end),e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
+xlabel('$\log(\Delta x)$ [-]');
 ylabel('$e_1$ [-]');
 title('Convergence analysis - using semilogx');
 grid;
 
 % 5.3. Using semilogy():
 subplot(2, 2, 3);
-semilogy(dx_list,e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
+semilogy(dx_list(2:end),e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
 xlabel('$\Delta x$ [-]');
-ylabel('$e_1$ [-]');
+ylabel('$\log(e_1)$ [-]');
 title('Convergence analysis - using semilogy');
 grid;
 
 % 5.4. Using loglog():
 subplot(2, 2, 4);
-loglog(dx_list,e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
-xlabel('$\Delta x$ [-]');
-ylabel('$e_1$ [-]');
+loglog(dx_list(2:end),e_1, 'x--', 'color', 'black', 'LineWidth', 1.5)
+xlabel('$\log(\Delta x)$ [-]');
+ylabel('$log(e_1)$ [-]');
 title('Convergence analysis - using loglog');
 grid;
 
@@ -119,6 +98,6 @@ grid;
 %% 6. Saving our figure as 'convergence2.png': --------------------------- %
 
 disp('Saving the figure as convergence2.png...');
-saveas(fig, 'convergence2_false.png');
+saveas(fig, 'convergence2.png');
 
 disp('Figured saved!');
